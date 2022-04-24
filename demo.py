f6d126a36e8eae741e7e29d7e69aeed4d6f9306b
Lee Magnusson <lee@unhuman.io>
2022-04-24 07:05:17 -0700

    plot

--- demo.py
+++ demo.py
@@ -3,7 +3,7 @@
 import sys
 sys.path.append("/usr/share/motor-realtime")
 
-from PyQt5.QtCore import QTimer, Qt, QMargins
+from PyQt5.QtCore import QTimer, Qt, QMargins, QCoreApplication
 from PyQt5.QtWidgets import (
     QApplication,
     QLabel,
@@ -17,6 +17,7 @@ from PyQt5.QtWidgets import (
     QLineEdit,
 )
 from PyQt5.QtGui import QPalette, QColor, QDoubleValidator
+from PyQt5.QtChart import QChart, QChartView, QLineSeries, QValueAxis
 import motor
 
 import signal
@@ -106,6 +107,54 @@ class Position(QWidget):
         motor_manager.set_command_mode(motor.ModeDesired.Position)
         motor_manager.write_saved_commands()
 
+class Plot(QWidget):
+    global motor_manager
+
+    def __init__(self):
+        super(Plot, self).__init__()
+        self.setAutoFillBackground(True)
+        self.name = "plot"
+
+        self.chart = QChart()
+        self.chart_view = QChartView(self.chart)
+        self.layout = QHBoxLayout()
+        self.layout.addWidget(self.chart_view)
+        self.setLayout(self.layout)
+
+        self.series = QLineSeries()
+        self.chart.addSeries(self.series)
+        self.axis_x = QValueAxis()
+        self.axis_x.setTickCount(10)
+        self.axis_x.setTitleText("Time (s)")
+        self.chart.addAxis(self.axis_x, Qt.AlignmentFlag.AlignBottom)
+        self.series.attachAxis(self.axis_x)
+        self.axis_y = QValueAxis()
+        self.axis_y.setTickCount(10)
+        self.axis_y.setTitleText("Value")
+        self.chart.addAxis(self.axis_y, Qt.AlignmentFlag.AlignLeft)
+        self.series.attachAxis(self.axis_y)
+        self.axis_y.setRange(-10,10)
+
+        s = motor_manager.read()[0]
+        self.mcu_timestamp = s.mcu_timestamp
+        self.t_seconds = 0.0
+        self.series.append(0, s.iq)
+
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update)
+        self.timer.start(10)
+
+
+    def update(self):
+        s = motor_manager.read()[0]
+        self.t_seconds += (motor.diff_mcu_time(s.mcu_timestamp, self.mcu_timestamp))/170e6
+        self.mcu_timestamp = s.mcu_timestamp
+        self.series.append(self.t_seconds, s.iq)
+        if len(self.series) > 500:
+            self.series.remove(0)
+        self.axis_x.setMax(self.t_seconds)
+        self.axis_x.setMin(self.series.at(0).x())
+
 class MainWindow(QMainWindow):
 
     def __init__(self):
@@ -121,12 +170,14 @@ class MainWindow(QMainWindow):
 
         self.tabs.addTab(DataDisplay(), "data")
         self.tabs.addTab(Position(), "position")
+        self.tabs.addTab(Plot(), "plot")
 
         self.setCentralWidget(self.tabs)
         self.last_tab = self.tabs.currentWidget()
 
         self.tabs.currentChanged.connect(self.new_tab)
-        self.showFullScreen()
+        if "-fullscreen" in QCoreApplication.arguments():
+            self.showFullScreen()
 
     def new_tab(self, index):
         print("last tab " + str(index) + " " + self.last_tab.name)
