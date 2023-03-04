a8fc7192232796f89caf6238cce4ede2bcf4a15d
Lee Magnusson <lee@unhuman.io>
2023-03-04 11:12:58 -0800

    motor select

--- faults.py
+++ faults.py
@@ -17,6 +17,8 @@ from PyQt5.QtWidgets import (
     QLineEdit,
     QSlider,
     QComboBox,
+    QMenuBar,
+    QMenu,
 )
 from PyQt5.QtGui import QPalette, QColor, QDoubleValidator
 from PyQt5.QtChart import QChart, QChartView, QLineSeries, QValueAxis
@@ -38,7 +40,7 @@ class StatusDisplay(QPushButton):
         pass
 
 class StatusCombo(QWidget):
-    signal = pyqtSignal(float)
+    signal = pyqtSignal(str, str)
     def __init__(self, *args, **kwargs):
         super(StatusCombo, self).__init__(*args, **kwargs)
         
@@ -55,12 +57,13 @@ class StatusCombo(QWidget):
         self.text_widget.setText(str(number))
 
     def editingFinished(self):
-        self.signal.emit(float(self.text_widget.text()))
+        self.signal.emit(self.combo_box.currentText(), self.text_widget.text())
 
 class MotorTab(QWidget):
     def __init__(self, *args, **kwargs):
         super(MotorTab, self).__init__(*args, **kwargs)
         self.setAutoFillBackground(True)
+        self.update_time = 100
 
         self.timer = QTimer()
         self.timer.timeout.connect(self.update)
@@ -74,7 +77,7 @@ class MotorTab(QWidget):
         self.timer.stop()
 
     def unpause(self):
-        self.timer.start(100)
+        self.timer.start(self.update_time)
 
 class FaultTab(MotorTab):
     def __init__(self, *args, **kwargs):
@@ -130,14 +133,74 @@ class StatusTab(MotorTab):
         for i in range(5):
             self.statuses.append(StatusCombo())
             layout.addWidget(self.statuses[i])
+            self.statuses[i].signal.connect(self.valueEdit)
             self.statuses[i].combo_box.addItems(self.field_names)
         self.setLayout(layout)
+
+
+    def valueEdit(self, s, v):
+        print("value edit: {}={}".format(s,v))
+        if v:
+            motor_manager.motors()[0][s] = v
 
     def update(self):
         super(StatusTab, self).update()
         for status in self.statuses:
-            val = motor_manager.motors()[0][status.combo_box.currentText()].get()
-            status.setText(val)
+            if not status.text_widget.hasFocus():
+                val = motor_manager.motors()[0][status.combo_box.currentText()].get()
+                status.setText(val)
+
+
+class PlotTab(MotorTab):
+    global motor_manager
+
+    def __init__(self):
+        super(PlotTab, self).__init__()
+
+        self.name = "plot"
+        self.update_time = 10
+
+        self.chart = QChart()
+        self.chart_view = QChartView(self.chart)
+        self.chart_view.setRubberBand(QChartView.VerticalRubberBand)
+        self.layout = QVBoxLayout()
+        self.combo_box = QComboBox()
+        self.combo_box.addItems(["motor_position", "joint_position", "iq"])
+        self.layout.addWidget(self.combo_box)
+        self.layout.addWidget(self.chart_view)
+        self.setLayout(self.layout)
+
+        self.series = QLineSeries()
+        self.chart.addSeries(self.series)
+        self.axis_x = QValueAxis()
+        self.axis_x.setTickCount(10)
+        self.axis_x.setTitleText("Time (s)")
+        self.chart.addAxis(self.axis_x, Qt.AlignmentFlag.AlignBottom)
+        self.series.attachAxis(self.axis_x)
+        self.axis_y = QValueAxis()
+        self.axis_y.setTickCount(10)
+        self.axis_y.setTitleText("Value")
+        self.chart.addAxis(self.axis_y, Qt.AlignmentFlag.AlignLeft)
+        self.series.attachAxis(self.axis_y)
+        self.axis_y.setRange(-100,100)
+
+        s = motor_manager.read()[0]
+        self.mcu_timestamp = s.mcu_timestamp
+        self.t_seconds = 0.0
+        self.series.append(0, getattr(s, self.combo_box.currentText()))
+
+
+    def update(self):
+        super(PlotTab, self).update()
+        s = self.status
+        self.t_seconds += (motor.diff_mcu_time(s.mcu_timestamp, self.mcu_timestamp))/170e6
+        self.mcu_timestamp = s.mcu_timestamp
+        self.series.append(self.t_seconds, getattr(s, self.combo_box.currentText()))
+        if len(self.series) > 500:
+            self.series.remove(0)
+        self.axis_x.setMax(self.t_seconds)
+        self.axis_x.setMin(self.series.at(0).x())
+
 
 class MainWindow(QMainWindow):
 
@@ -145,15 +208,24 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
         global motor_manager
         motor_manager = motor.MotorManager()
-        motors = motor_manager.get_connected_motors()
-        m1 = motors[0]
+        motors = motor_manager.get_connected_motors(connect=False)
+        print(motors)
+        self.menu_bar = QMenuBar(self)
+        self.motor_menu = QMenu("&Motor")
+        self.menu_bar.addMenu(self.motor_menu)
+        self.setMenuBar(self.menu_bar)
+        for m in motors:
+            action = self.motor_menu.addAction(m.name())
+            action.triggered.connect(lambda: self.connect_motor(m.name()))
+        self.connect_motor(motors[0].name())
 
-        self.setWindowTitle(m1.name())
+
         self.tabs = QTabWidget()
         self.tabs.setTabPosition(QTabWidget.TabPosition.West)
 
         self.tabs.addTab(FaultTab(), "fault")
         self.tabs.addTab(StatusTab(), "status")
+        self.tabs.addTab(PlotTab(), "plot")
 
         self.setCentralWidget(self.tabs)
         self.last_tab = self.tabs.currentWidget()
@@ -162,6 +234,11 @@ class MainWindow(QMainWindow):
         self.tabs.currentChanged.connect(self.new_tab)
         if "-fullscreen" in QCoreApplication.arguments():
             self.showFullScreen()
+
+    def connect_motor(self, name):
+         print("Connecting motor " + name)
+         motor_manager.get_motors_by_name([name])
+         self.setWindowTitle(name)
 
     def new_tab(self, index):
         #print("last tab " + str(index) + " " + self.last_tab.name)
