6e228b97520e628fe18f57ab9af5a82803a9948d
Lee Magnusson <lee@unhuman.io>
2023-03-21 07:59:46 -0700

    current tuning frequency measurement

--- motor_gui.py
+++ motor_gui.py
@@ -684,40 +684,54 @@ class CurrentTuningTab(MotorTab):
         measure_layout.addWidget(self.freq_meas, 0, 1)
         self.phase = NumberDisplay("phase shift")
         measure_layout.addWidget(self.phase, 1, 0)
+        self.mag_des = NumberDisplay("magnitude des")
+        measure_layout.addWidget(self.mag_des, 1, 1)
         self.mag = NumberDisplay("magnitude")
-        measure_layout.addWidget(self.mag, 1, 1)
+        measure_layout.addWidget(self.mag, 1, 2)
         layout.addLayout(measure_layout)
         self.setLayout(layout)
-        t = np.matrix(np.linspace(0,94*(1/50000),95))
-        self.freq =np.matrix(np.linspace(1,10000,10000)).transpose()
-        self.ei = np.exp(1j*self.freq*t*2*np.pi)
+        #t = np.matrix(np.linspace(0,94*(1/50000),95))
+        self.freq =np.matrix(np.linspace(0,10000,10000//5+1)).transpose()
+        #self.ei = np.exp(1j*self.freq*t*2*np.pi)
 
     def update(self):
         super(CurrentTuningTab, self).update()
         fast_log = current_motor().get_fast_log()
         #self.text.setPlainText(fast_log)
-        data = np.genfromtxt(StringIO(fast_log), delimiter=",", names=True, skip_footer=1)
-        t_seconds = data["timestamp"]/cpu_frequency*1000
-        t_seconds -= t_seconds[0]
-        iq_des = np.matrix(data["iq_des"]).transpose()
-        iq_meas_filt = data["iq_meas_filt"]
-        dt = (t_seconds[1] - t_seconds[0])/1000
 
-        fmeas = fft(iq_des)
-        fmeas = self.ei*iq_des
-        
-        fmeasi = np.argmax(abs(fmeas))
-        #freq = fftfreq(iq_des.size, dt)
-        self.freq_des.setNumber(self.freq[fmeasi])
-        self.freq_meas.setNumber(fmeasi)
-        self.phase.setNumber(dt)
-        self.mag.setNumber(abs(fmeas[fmeasi])/95.*2)
 
 
         try:
+            data = np.genfromtxt(StringIO(fast_log), delimiter=",", names=True, skip_footer=1)
+            t_seconds = data["timestamp"]/cpu_frequency*1000
+            t_seconds -= t_seconds[0]
+            self.ei = np.exp(1j*self.freq*t_seconds/1000*2*np.pi)
+            iq_des = np.matrix(data["iq_des"]).transpose()
+            iq_meas_filt = np.matrix(data["iq_meas_filt"]).transpose()
+            dt = (t_seconds[1] - t_seconds[0])/1000
+
+            #fmeas = fft(iq_des)
+            fmeas = self.ei*iq_des
+            
+            fmeasi = np.argmax(abs(fmeas))
+            #freq = fftfreq(iq_des.size, dt)
+            self.freq_des.setNumber(self.freq[fmeasi])
+            mag_des = np.abs(fmeas[fmeasi])/iq_des.size*2
+            self.mag_des.setNumber(mag_des)
+            phas_des = np.angle(fmeas[fmeasi])
+
+            fmeas_meas = self.ei*iq_meas_filt
+            fmeas_mi = np.argmax(abs(fmeas_meas))
+            self.freq_meas.setNumber(self.freq[fmeas_mi])
+            mag_meas = np.abs(fmeas_meas[fmeasi])/iq_des.size*2
+            phas_meas = np.angle(fmeas_meas[fmeasi])
+
+            self.phase.setNumber(-(phas_meas - phas_des)*180/np.pi)
+            self.mag.setNumber(mag_meas/mag_des)
+
             xy = [QPointF(x[0],x[1]) for x in np.column_stack((t_seconds, np.array(iq_des)))]
             self.series.replace(xy) #append(self.t_seconds, float(val))
-            xy2 = [QPointF(x[0],x[1]) for x in np.column_stack((t_seconds, iq_meas_filt))] # abs(fmeas)))]#iq_meas_filt))]
+            xy2 = [QPointF(x[0],x[1]) for x in np.column_stack((t_seconds, np.array(iq_meas_filt)))] # abs(fmeas)))]#iq_meas_filt))]
             self.series2.replace(xy2)
             min_y = min(min([d.y() for d in self.series.pointsVector()]), min([d.y() for d in self.series2.pointsVector()]))
             max_y = max(max([d.y() for d in self.series.pointsVector()]), max([d.y() for d in self.series2.pointsVector()]))
