5a1c95b7ff41367711d2d7093a17404e31639f3f
Lee Magnusson <lee@unhuman.io>
2023-11-13 16:26:33 -0800

    switch to streamingchart

--- motor_gui.py
+++ motor_gui.py
@@ -3,7 +3,7 @@
 import sys
 import traceback
 
-from PyQt5.QtCore import QTimer, Qt, QMargins, QCoreApplication, pyqtSignal, QPointF
+from PyQt5.QtCore import QTimer, Qt, QMargins, QCoreApplication, pyqtSignal, QPointF, QEvent
 from PyQt5.QtWidgets import (
     QApplication,
     QLabel,
@@ -396,15 +396,15 @@ class PlotTab(MotorTab):
         self.name = "plot"
         self.update_time = 10
 
-        self.chart = QChart()
-        self.chart_view = QChartView(self.chart)
-        self.chart_view.setRubberBand(QChartView.VerticalRubberBand)
+        self.chart = StreamingChart()
         self.layout = QVBoxLayout()
         self.combo_box = QComboBox()
-        self.combo_box.addItems(["motor_position", "joint_position", "iq", "torque", "motor_encoder", "motor_velocity", "joint_velocity", "iq_desired", "reserved"])
-        self.combo_box.currentIndexChanged.connect(lambda: self.series.removePoints(0,self.series.count()))
+        self.combo_box.addItems(["motor_position", "joint_position", "iq", "torque", "motor_encoder", 
+            "motor_velocity", "joint_velocity", "iq_desired", "reserved", "encoder_error", "cogging"])
+        self.combo_box.currentIndexChanged.connect(self.chart.removePoints)
+        self.combo_box.currentIndexChanged.connect(self.name_change)
         self.layout.addWidget(self.combo_box)
-        self.layout.addWidget(self.chart_view)
+        self.layout.addWidget(self.chart)
         var_layout = QHBoxLayout()
         self.pp = NumberDisplay("peak-peak")
         var_layout.addWidget(self.pp)
@@ -415,24 +415,19 @@ class PlotTab(MotorTab):
         self.layout.addLayout(var_layout)
         self.setLayout(self.layout)
 
-        self.series = QLineSeries()
-        self.chart.addSeries(self.series)
-        self.axis_x = QValueAxis()
-        self.axis_x.setTickCount(10)
-        self.axis_x.setTitleText("Time (s)")
-        self.chart.addAxis(self.axis_x, Qt.AlignmentFlag.AlignBottom)
-        self.series.attachAxis(self.axis_x)
-        self.axis_y = QValueAxis()
-        self.axis_y.setTickCount(10)
-        self.axis_y.setTitleText("Value")
-        self.chart.addAxis(self.axis_y, Qt.AlignmentFlag.AlignLeft)
-        self.series.attachAxis(self.axis_y)
-        self.axis_y.setRange(-100,100)
 
         s = motor_manager.read()[0]
         self.mcu_timestamp = s.mcu_timestamp
         self.t_seconds = 0.0
-        self.series.append(0, getattr(s, self.combo_box.currentText()))
+
+    def name_change(self):
+        plot_str = self.combo_box.currentText()
+        print(plot_str)
+        if plot_str == "encoder_error":
+            self.gear_ratio = float(current_motor()["gear_ratio"].get())
+            self.chart.length=10000
+        else:
+            self.chart.length=500
 
 
     def update(self):
@@ -440,17 +435,23 @@ class PlotTab(MotorTab):
         s = self.status
         self.t_seconds += (motor.diff_mcu_time(s.mcu_timestamp, self.mcu_timestamp))/cpu_frequency
         self.mcu_timestamp = s.mcu_timestamp
-        self.series.append(self.t_seconds, getattr(s, self.combo_box.currentText()))
-        val = np.array([d.y() for d in self.series.pointsVector()])
+        plot_str = self.combo_box.currentText()
+        val = 0
+        x = self.t_seconds
+        if plot_str == "encoder_error":
+            val = s.motor_position - s.joint_position * self.gear_ratio
+        elif plot_str == "cogging":
+            val = s.iq
+            x = np.mod(s.motor_position, 2*np.pi)
+        else:
+            val = getattr(s, plot_str)
+        
+        self.chart.update(x, [val])
+
+        val = np.array([d.y() for d in self.chart.series[0].pointsVector()])
         self.std.setNumber(np.std(val))
         self.mean.setNumber(np.mean(val))
         self.pp.setNumber(max(val) - min(val))
-        if len(self.series) > 500:
-            self.series.remove(0)
-        self.axis_x.setMax(self.t_seconds)
-        self.axis_x.setMin(self.series.at(0).x())
-        self.axis_y.setMin(min([d.y() for d in self.series.pointsVector()]))
-        self.axis_y.setMax(max([d.y() for d in self.series.pointsVector()]))
 
 
 class PlotTab2(MotorTab):
@@ -462,16 +463,14 @@ class PlotTab2(MotorTab):
         self.name = "plot2"
         self.update_time = 10
 
-        self.chart = QChart()
-        self.chart_view = QChartView(self.chart)
-        self.chart_view.setRubberBand(QChartView.VerticalRubberBand)
+        self.chart = StreamingChart()
         self.layout = QVBoxLayout()
         self.combo_box = QComboBox()
         self.field_names = sorted(current_motor().get_api_options())
         self.combo_box.addItems(self.field_names)
         self.layout.addWidget(self.combo_box)
         self.combo_box.setCurrentText("vbus")
-        self.layout.addWidget(self.chart_view)
+        self.layout.addWidget(self.chart)
         var_layout = QHBoxLayout()
         self.pp = NumberDisplay("peak-peak")
         var_layout.addWidget(self.pp)
@@ -483,22 +482,7 @@ class PlotTab2(MotorTab):
         self.setLayout(self.layout)
         self.setLayout(self.layout)
 
-        self.series = QLineSeries()
-        self.series.setUseOpenGL(True)
-        self.chart.addSeries(self.series)
-        self.axis_x = QValueAxis()
-        self.axis_x.setTickCount(10)
-        self.axis_x.setTitleText("Time (s)")
-        self.chart.addAxis(self.axis_x, Qt.AlignmentFlag.AlignBottom)
-        self.series.attachAxis(self.axis_x)
-        self.axis_y = QValueAxis()
-        self.axis_y.setTickCount(10)
-        self.axis_y.setTitleText("Value")
-        self.chart.addAxis(self.axis_y, Qt.AlignmentFlag.AlignLeft)
-        self.series.attachAxis(self.axis_y)
-        self.axis_y.setRange(-100,100)
-
-        self.combo_box.currentIndexChanged.connect(lambda: self.series.removePoints(0,self.series.count()))
+        self.combo_box.currentIndexChanged.connect(self.chart.removePoints)
 
         s = motor_manager.read()[0]
         self.mcu_timestamp = s.mcu_timestamp
@@ -514,17 +498,11 @@ class PlotTab2(MotorTab):
         self.mcu_timestamp = s.mcu_timestamp
         val = current_motor()[self.combo_box.currentText()].get()
         try:
-            self.series.append(self.t_seconds, float(val))
-            val2 = np.array([d.y() for d in self.series.pointsVector()])
+            self.chart.update(self.t_seconds, float(val))
+            val2 = np.array([d.y() for d in self.chart.series[0].pointsVector()])
             self.std.setNumber(np.std(val2))
             self.mean.setNumber(np.mean(val2))
             self.pp.setNumber(max(val2) - min(val2))
-            if len(self.series) > 500:
-                self.series.remove(0)
-            self.axis_y.setMin(min([d.y() for d in self.series.pointsVector()]))
-            self.axis_y.setMax(max([d.y() for d in self.series.pointsVector()]))
-            self.axis_x.setMax(self.t_seconds)
-            self.axis_x.setMin(self.series.at(0).x())
         except ValueError:
             pass
 
@@ -939,6 +917,17 @@ class BodeWindow(QWidget):
             self.axis_y.setTitleText("magnitude (20*log10(mag))")
             self.chart.addAxis(self.axis_y, Qt.AlignmentFlag.AlignLeft)
             self.series.attachAxis(self.axis_y)
+
+            self.chart_view.setMouseTracking(True)
+            self.chart_view.viewport().installEventFilter(self)
+
+        def eventFilter(self, obj, event):
+            if obj is self.chart_view.viewport() and event.type() == QEvent.MouseMove:
+                lp = event.pos()
+                sp = self.chart_view.mapToScene(lp)
+                vp = self.chart_view.chart().mapToValue(sp)
+                print(vp)
+            return super().eventFilter(obj, event)
             
             
 
@@ -1190,7 +1179,8 @@ class StreamingChart(QChartView):
         self.chart = QChart()
         super(StreamingChart, self).__init__(self.chart, *args, **kwargs)
         self.setRubberBand(QChartView.VerticalRubberBand)
-        self.num_lines = num_lines  
+        self.num_lines = num_lines
+        self.length = 500
         
         self.axis_x = QValueAxis()
         self.axis_x.setTickCount(10)
@@ -1217,17 +1207,21 @@ class StreamingChart(QChartView):
             max1 = float("-inf")
             for i in range(self.num_lines):
                 self.series[i].append(t, data[i])
-                if len(self.series[i]) > 200:
+                if len(self.series[i]) > self.length:
                     self.series[i].remove(0)
                 min1 = min(min1,min([d.y() for d in self.series[i].pointsVector()]))
                 max1 = max(max1,max([d.y() for d in self.series[i].pointsVector()]))
 
             self.axis_y.setMin(min1)
             self.axis_y.setMax(max1)
-            self.axis_x.setMax(t)
-            self.axis_x.setMin(self.series[0].at(0).x())
+            self.axis_x.setMax(max([d.x() for d in self.series[0].pointsVector()]))
+            self.axis_x.setMin(min([d.x() for d in self.series[0].pointsVector()]))
         except ValueError:
             pass
+    
+    def removePoints(self):
+        for i in range(self.num_lines):
+            self.series[i].removePoints(0,self.series[i].count())
 
 class PositionTuningTab(MotorTab):
     def __init__(self, *args, **kwargs):
@@ -1335,9 +1329,11 @@ class PositionTuningTab(MotorTab):
 
     def mode_update(self, selection):
         self.command.position_tuning.mode = int(motor.TuningMode.__members__[selection])
-        # if self.command.position_tuning.mode == motor.TuningMode.Chirp:
-        #     self.bode_window = BodeWindow()
-        #     self.bode_window.show()
+        if self.command.position_tuning.mode == motor.TuningMode.Chirp:
+            self.bode_window = BodeWindow()
+            self.bode_window.set_frequency_limits(1, 500)
+            self.bode_window.set_magnitude_limits(-40,10)
+            self.bode_window.show()
         self.command_update()
 
 app = QApplication(sys.argv)
